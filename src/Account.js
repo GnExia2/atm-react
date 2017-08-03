227061e0e79374c500d64bd8746b2cf45123c12a
YOUR_GITHUB_USERNAME <garrickdark@yahoo.com>
2017-08-03 10:05:11 -0700

    finished HW

--- src/Account.js
+++ src/Account.js
@@ -1,15 +1,52 @@
 import React, { Component } from 'react';
 
 export default class Account extends Component {
+    constructor(props){
+      super(props)
+      this.state = {
+        balance: 0
+      }
+      this.handleDepositClick = this.handleDepositClick.bind(this)
+      this.handleWithdrawalClick = this.handleWithdrawalClick.bind(this)
+    }
+
+    handleDepositClick(e) {
+      e.preventDefault()
+      let amount = parseInt(this.refs.amount.value, 10);
+      let newBalance = this.state.balance + amount;
+      this.setState({
+        balance: newBalance
+      })
+      this.refs.amount.value = '';
+    }
+
+    handleWithdrawalClick(e) {
+      e.preventDefault()
+      let amount = parseInt(this.refs.amount.value, 10);
+      let newBalance = this.state.balance - amount;
+      if (newBalance < 0) {
+        newBalance = 0
+      }
+      this.setState({
+        balance: newBalance
+      })
+      this.refs.amount.value = '';
+    }
+
   render() {
     return (
       <div className="account">
-        <h2>TODO: CHANGE TO PROP</h2>
-        <div className="balance">$0</div>
-        <input type="text" placeholder="enter an amount" />
-        <input type="button" value="Deposit" />
-        <input type="button" value="Withdraw" />
+        <h2>{this.props.name}</h2>
+        <div className="balance">${this.state.balance}</div>
+        <input type="text" placeholder="enter an amount" ref="amount"/>
+        <input type="button" value="Deposit" onClick={this.handleDepositClick} />
+        <input type="button" value="Withdraw" onClick={this.handleWithdrawalClick} />
       </div>
     )
   }
 }
+
+
+
+//why does program not work when functions are outside constructor
+//what does the bind lines do?
